46ffcd6949c9ed20139970cba3f19f67e01f959a
ascenthurera <mah786447@gmail.com>
2026-03-17 05:18:19 +0500

    Javascript Fundamentals

---
+++ 11/hello.js
@@ -0,0 +1,28 @@
+//Javascript is a programming language that is used to create interactive effects within web browsers. 
+//It is a versatile language that can be used for both front-end and back-end development.
+
+console.log("Hello")
+let lastName = "Ali";
+let _x = 2;
+const carName = "BYD"
+
+const names = ["Ali", "Hamza"] //array
+console.log(names[0])
+
+//Defining an object
+const studentNames = {
+    first: "Usama",
+    last: "Gaffar"
+};
+console.log(studentNames.first)
+
+const number1 = 5;
+const number2 = 10;
+console.log(number1 == number2) //False
+
+const number3 = 5;
+const number4 = 7;
+console.log(number3 != number4) //True
+console.log(number3 && number4) //True
+console.log(number3 || number4) //True
+console.log(!number3) //False
